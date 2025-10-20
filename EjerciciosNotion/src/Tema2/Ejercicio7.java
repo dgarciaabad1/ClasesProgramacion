e1e4f3f6389197092b770ef55fcbc0f8d2754ff8
David García Abad <d.garciaabad1@gmail.com>
2025-10-20 12:06:07 +0200

    Update Ejercicio7.java

--- EjerciciosNotion/src/Tema2/Ejercicio7.java
+++ EjerciciosNotion/src/Tema2/Ejercicio7.java
@@ -1,5 +1,7 @@
 package Tema2;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
     public static void main (String[] args){
@@ -11,7 +13,20 @@ public class Ejercicio7 {
         DESCUENTO ESTUDIANTE / DESCUENTO ESPECIAL (MENOR 26 + ESTUDIANTE)
          */
 
+        System.out.println("Introduce tu edad");
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextInt();
+
+        System.out.println("¿Eres estudiante?");
+        Scanner sc2 = new Scanner(System.in);
+        boolean estudiante = sc2.nextBoolean();
+
         
+        System.out.println("Eres estudiante: " + estudiante);
+        System.out.println("NO eres estudiante: " + (!estudiante));
+        System.out.println("Tienes descuento joven: " + (edad<=26));
+        System.out.println("Tienes descuento de estudiante: " + estudiante);
+        System.out.println("Tienes descuento especial:" + (estudiante&&(edad<=26)));
 
     }
 }
