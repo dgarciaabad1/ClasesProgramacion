57299988e6c2853bdecf68a3093773a0f227e880
David García Abad <d.garciaabad1@gmail.com>
2025-10-21 11:41:27 +0200

    Update Ejercicio8.java

--- EjerciciosNotion/src/Tema2/Ejercicio8.java
+++ EjerciciosNotion/src/Tema2/Ejercicio8.java
@@ -1,5 +1,7 @@
 package Tema2;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
     static void main (String[] args){
@@ -9,6 +11,29 @@ public class Ejercicio8 {
     LA SUMA / EL PROMEDIO / RESULTADO MULTIPLICCAR PRIMERO POR SEGUNDO Y DIVIDIRLO POR EL TERCERO
      */
 
+        System.out.println("Introduce el primer numero");
+        Scanner sc1 = new Scanner (System.in);
+        int num1 = sc1.nextInt();
+
+        System.out.println("Introduce el segundo numero");
+        Scanner sc2 = new Scanner (System.in);
+        int num2 = sc2.nextInt();
+
+        System.out.println("Introduce el tercer numero");
+        Scanner sc3 = new Scanner (System.in);
+        int num3 = sc3.nextInt();
+
+        int total = (num1+num2+num3);
+        System.out.println("Suma de los tres numeros: " + total);
+        //suma total de los 3 numeros
+
+        int promedio = ((num1+num2+num3)/3);
+        //aqui tambien se podria haber hecho int promedio = total/3; pero el ejercicio pide que se usen paréntesis
+        System.out.println("Promedio: " + promedio);
+        //calcular promedio
+
+        int operacion = ((num1*num2)/num3);
+        System.out.println("Resultado de (" + num1 + " * " + num2 + ") / " + num3 + ": " + operacion);
 
     }
 }
